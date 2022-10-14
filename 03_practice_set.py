c400f54cb1b070d1e1c0ec16c9f3701ce4b61e78
Imran Khan <107785890+imran4415@users.noreply.github.com>
2022-10-14 09:49:06 +0530

    Add files via upload

---
+++ 03_practice_set.py
@@ -0,0 +1,26 @@
+# name = input("Enter name: ")
+# print("Good after noon! ",name)
+
+
+# letter = '''Dear <|NAME|>,
+# You are selected!
+#         Date:     <|DATE|>
+# '''
+# name = input("Enter name: ")
+# date = input("Enter date: ")
+
+# letter = letter.replace("<|NAME|>", name)
+# letter = letter.replace("<|DATE|>", date)
+# print(letter)
+
+
+# str = "I am here to  learn  the python programming."
+
+# doubleSpaces = str.find("  ") # other method to do this and then print doubleSpaces
+
+# print(str.find("  "))
+# print(str.replace("  "," "))
+
+
+letter = "Dear Imran, \n\tThis python course is nice. \n\t\t\tThanks!"
+print(letter)
